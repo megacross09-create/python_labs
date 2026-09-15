4824190d3d701d80384392ea4a7138d6872a9a40
megacross09-create <megacross09@gmail.com>
2026-09-15 13:56:40 +0300

    hzz

---
+++ src/lab01/02_sum_avg.py
@@ -0,0 +1,2 @@
+a, b = (float(input().replace(',','.'))), (float(input().replace(',','.')))
+print(f'sum={(a+b):.2f}; avg={((a+b)/2):.2f}')
---
+++ src/lab01/03_discount_vat.py
@@ -0,0 +1,7 @@
+pr, disc, vat = float(input()), float(input()), float(input())
+base = pr * (1-disc/100)
+vat_am = base * (vat/100)
+tot = base + vat_am
+print(f'База после скидки: {(base):.2f} ₽\n'
+      f'НДС:               {(vat_am):.2f} ₽\n'
+      f'Итого к оплате:    {(tot):.2f} ₽')
