ee776455640674b7368c42f4b56755e630ea6dab
megacross09-create <megacross09@gmail.com>
2026-09-23 15:36:47 +0300

    newex5

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,3 +1,6 @@
 q = str(input("ФИО: "))
-print(f'Инициалы: {(''.join([i[:1] for i in q.split()])).upper()}.\nДлина (символов): {' '.join(q.split()).count('')-1}')
-# надо трайнуть генератором иницаилы вырезат и всес
+# print(f'Инициалы: {(''.join([i[:1] for i in q.split()])).upper()}.\nДлина (символов): {' '.join(q.split()).count('')-1}')
+# # надо трайнуть генератором иницаилы вырезат и всес
+init=''.join([i[:1] for i in q.split()])
+length = ' '.join(q.split()).count('')-1
+print(f'Инициалы: {init}.\nДлина (символов): {length}')
